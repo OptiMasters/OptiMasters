1360ee1e82de478f57d544a58abc6e49dbde3ef8
OptiMasters <OptiMasters@gmail.com>
2024-04-17 10:31:00 +0900

    settings 변경

--- config/settings/base.py
+++ config/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,16 +79,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'test_db3',
-        'USER': 'root',
-        'PASSWORD': '1234',
-        'HOST':'localhost',
-        'PORT':'3306',
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'test_db3',
+        # 'USER': 'root',
+        # 'PASSWORD': '1234',
+        # 'HOST':'localhost',
+        # 'PORT':'3306',
     # },
     # 'sqlite':{
-    #     'ENGINE':'django.db.backends.sqlite3',
-    #     'NAME':BASE_DIR / 'db.sqlite3',
+        'ENGINE':'django.db.backends.sqlite3',
+        'NAME':BASE_DIR / 'db.sqlite3',
     }
 }
 
